342d2936fc42f9639bd8c7370ea495897bc24323
tamtale <tamtale@gmail.com>
2020-01-13 15:29:04 -0600

    init

---
+++ desktop/src/com/week1/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.week1.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.week1.game.Week1Demo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Week1Demo(), config);
+	}
+}
