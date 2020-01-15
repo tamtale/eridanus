4dd21d55dfb9daf1c802cf4407898b80efd6176e
tamtale <tamtale@gmail.com>
2020-01-15 15:54:07 -0600

    bases loaded

--- core/src/com/week1/game/Week1Demo.java
+++ core/src/com/week1/game/Week1Demo.java
@@ -2,32 +2,85 @@ package com.week1.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Week1Demo extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Viewport viewport;
+	private OrthographicCamera camera;
+	private Rectangle base1;
+	private Rectangle base2;
+	private Array<Rectangle> units;
+	private Pixmap unitPixmap;
+	private Texture unitTexture;
+	private Sprite unitSprite;
+	private Pixmap basePixmap;
+	private Sprite baseSprite;
+	Vector3 touchPos = new Vector3();
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		units = new Array<>();
+	    batch = new SpriteBatch();
+
+	    basePixmap = new Pixmap(64, 64, Pixmap.Format.RGB888);
+	    basePixmap.setColor(Color.RED);
+	    basePixmap.fill();
+	    img = new Texture(basePixmap);
+	    basePixmap.dispose();
+	    baseSprite = new Sprite(img);
+	    base1 = new Rectangle();
+	    base2 = new Rectangle();
+	    base1.x = 0;
+	    base1.y = Gdx.graphics.getHeight() - 64;
+		base2.x = Gdx.graphics.getWidth() - 64;
+		base2.y = 0;
+
+		unitPixmap = new Pixmap(32, 32, Pixmap.Format.RGB888);
+		unitPixmap.setColor(Color.BLUE);
+		unitPixmap.fill();
+		unitTexture = new Texture(unitPixmap);
+		unitSprite = new Sprite(unitTexture);
+
+		units.add(new Rectangle(100, 100, 32, 32));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		baseSprite.setPosition(base1.x, base1.y);
+		baseSprite.draw(batch);
+		baseSprite.setPosition(base2.x, base2.y);
+		baseSprite.draw(batch);
+		for (Rectangle unit: units) {
+			unitSprite.setPosition(unit.x, unit.y);
+			unitSprite.draw(batch);
+		}
 		batch.end();
+
+		if (Gdx.input.isTouched()) {
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+		}
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
 	}
 }
+
+class Unit {
+	private int dx;
+	private int dy;
+}
+
--- desktop/src/com/week1/game/desktop/DesktopLauncher.java
+++ desktop/src/com/week1/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.week1.game.Week1Demo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "RTS";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new Week1Demo(), config);
 	}
 }
