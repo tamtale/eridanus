03bfca09f7aff4beff499f72dbe7d15a6c8b74ad
Patrick Brady <pj.brady97@gmail.com>
2020-01-20 15:30:52 -0600

    Added info to the Message types, added more logic to game state

--- core/src/com/week1/game/Model/GameState.java
+++ core/src/com/week1/game/Model/GameState.java
@@ -1,13 +1,28 @@
 package com.week1.game.Model;
 
+import com.badlogic.gdx.utils.Array;
+import com.week1.game.SteeringAgent;
+
 public class GameState {
 
-    public GameState(){
-        // Units
-        // board
-        // player data
-        // towers
+    private Array<Unit> units;
+    private Array<SteeringAgent> agents;
 
+    public GameState(){
+        // TODO board
+        // TODO player data
+        // TODO towers
+        // TODO tower types in memory after exchange
+
+        units = new Array<>();
+        agents = new Array<>();
     }
 
+    public void addUnit(Unit u){
+        units.add(u);
+    }
+
+    public void addAgent(SteeringAgent a){
+        agents.add(a);
+    }
 }
