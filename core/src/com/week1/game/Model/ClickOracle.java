f69a135afba60a4d85dcecb1abffedad5818ef7c
tamtale <tamtale@gmail.com>
2020-01-20 15:34:39 -0600

    migrate renderer things

--- core/src/com/week1/game/Model/ClickOracle.java
+++ core/src/com/week1/game/Model/ClickOracle.java
@@ -1,7 +1,52 @@
 package com.week1.game.Model;
 
-public class ClickOracle {
-    public void click(float x, float y) {
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ai.steer.behaviors.Arrive;
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
+public class ClickOracle extends InputAdapter {
+
+    private Vector3 touchPos;
+    private IClickOracle2RendererAdapter rendererAdapter;
+    private IClickOracle2EngineAdapter engineAdapter;
+    private Unit selected;
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+
+        touchPos.set(screenX, screenY, 0);
+        rendererAdapter.unproject(touchPos);
+
+        if (button == Input.Buttons.LEFT) {
+            selected = engineAdapter.selectUnit(touchPos);
+            if (selected == null) {
+            }
+
+            select(engineAdapter.spawn(touchPos));
+            return true;
+        }
+        // Right click
+        if (selected != null) {
+            // TODO: steering agent behavior
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    private void select(Unit unit) {
+        unselect();
+        selected = unit;
+        unit.clicked = true;
+    }
+    private void unselect() {
+        if (selected != null) {
+            selected.clicked = false;
+        }
+        selected = null;
     }
 }
--- core/src/com/week1/game/Renderer/Renderer.java
+++ core/src/com/week1/game/Renderer/Renderer.java
@@ -1,68 +1,55 @@
 package com.week1.game.Renderer;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
 import com.week1.game.Model.GameState;
 import com.week1.game.Model.Unit;
-import com.week1.game.SteeringAgent;
 
 public class Renderer {
 
     public static int SCALE = 8; // 8 pixels per unit.
-    public static int SPEED = 8;
     private Batch batch;
     private OrthographicCamera camera;
-    private Array<Unit> units;
     private Pixmap unitPixmap;
     private Pixmap unitPixmap2;
     private Texture unitTexture;
     private Texture unitTexture2;
     private Vector3 touchPos = new Vector3();
     private TiledMap map;
-    private OrthogonalTiledMapRenderer renderer;
-    private Unit selected;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private IRenderer2EngineAdapter adapter;
 
     public void create(GameState state) {
         map = new TmxMapLoader().load("testmap.tmx");
         camera = new OrthographicCamera();
-        renderer = new OrthogonalTiledMapRenderer(map, 1f / SCALE);
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 1f / SCALE);
         camera.setToOrtho(false, 256, 256);
         camera.update();
-    }
+        unitPixmap = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888);
 
-    public void step(float delta) {
-        for(Unit unit: units) {
-            //System.out.println("from step " + agent.getSteeringOutput().linear);
-            unit.step(delta);
-        }
+        unitPixmap.setColor(Color.BLUE);
+        unitPixmap.fill();
 
+        unitPixmap2 = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888);
+        unitPixmap2.setColor(Color.RED);
+        unitPixmap2.fill();
+
+        unitTexture = new Texture(unitPixmap);
+        unitTexture2 = new Texture(unitPixmap2);
     }
 
     public void render(GameState state) {
         float diff = Gdx.graphics.getDeltaTime();
-        step(diff);
         Gdx.gl.glClearColor(0, 1f, 1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
-        renderer.setView(camera);
-        renderer.render();
-        batch.begin();
-        for (Unit unit: units) {
-            if (unit.clicked) {
-                batch.draw(unitTexture, unit.x, unit.y);
-            } else {
-                batch.draw(unitTexture2, unit.x, unit.y);
-            }
-        }
-        batch.end();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+        adapter.drawUnits(batch);
     }
 }
