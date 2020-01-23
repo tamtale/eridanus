65eecb5dc7960efff71bb41dd76d73c72ac9eafa
tamtale <tamtale@gmail.com>
2020-01-23 15:01:25 -0600

    Cleanup unit
    - remove Pixmap/texture creation on every instance (can instead use a static variable, can prob make this a little clearner with a config class)

--- core/src/com/week1/game/Model/Unit.java
+++ core/src/com/week1/game/Model/Unit.java
@@ -9,26 +9,25 @@ import com.week1.game.SteeringAgent;
 import static com.week1.game.GameController.SCALE;
 
 public class Unit extends Rectangle {
-    public boolean clicked;
+    public boolean clicked = false;
     public SteeringAgent agent;
-    public Texture unselectedSkin, selectedSkin;
-    private Pixmap unitPixmap, unitPixmap2;
+    private static Pixmap unitPixmap = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888){{
+       setColor(Color.BLUE);
+       fill();
+    }};
+    private static Pixmap unitPixmap2 = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888){{
+        setColor(Color.RED);
+        fill();
+    }};
+    private static Texture unselectedSkin = new Texture(unitPixmap){{
+        unitPixmap.dispose();
+    }};
+    private static Texture selectedSkin = new Texture(unitPixmap2){{
+        unitPixmap2.dispose();
+    }};
 
     public Unit(float x, float y) {
         super(x, y, SCALE, SCALE);
-        this.x = x;
-        this.y = y;
-        this.clicked = false;
-        // TODO put Pixmap creation somewhere at game creation. only one is necessary per unit type.
-        unitPixmap = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888);
-        unitPixmap.setColor(Color.BLUE);
-        unitPixmap.fill();
-        this.unselectedSkin = new Texture(unitPixmap);
-
-        unitPixmap2 = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888);
-        unitPixmap2.setColor(Color.RED);
-        unitPixmap2.fill();
-        this.selectedSkin = new Texture(unitPixmap2);
     }
 
     public void step(float delta) {
