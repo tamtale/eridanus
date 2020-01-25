5e7c10b7b997d2a05b849cde0d5ed4467dba46ac
warofthewrens <42586441+warofthewrens@users.noreply.github.com>
2020-01-25 17:35:35 -0600

    merged to master

---
+++ core/src/com/week1/game/Model/PlayerBase.java
@@ -0,0 +1,31 @@
+package com.week1.game.Model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
+
+public class PlayerBase {
+    public float x, y;
+    private float hp;
+    private int playerID;
+    private Texture skin;
+    private final int SIDELENGTH = 32;
+
+    public PlayerBase(float initialHp, float x, float y, int playerID) {
+
+        Pixmap towerUnscaled = new Pixmap(Gdx.files.internal("basetop.png"));
+        Pixmap towerScaled = new Pixmap(SIDELENGTH, SIDELENGTH, Pixmap.Format.RGBA8888);
+        towerScaled.drawPixmap(towerUnscaled, 0, 0, towerUnscaled.getWidth(), towerUnscaled.getHeight(), 0, 0, SIDELENGTH, SIDELENGTH);
+
+        this.skin = new Texture(towerScaled);
+        this.hp = initialHp;
+        this.playerID = playerID;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Texture getSkin() {
+        return skin;
+    }
+}
---
+++ core/src/com/week1/game/Model/Tower.java
@@ -0,0 +1,35 @@
+package com.week1.game.Model;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
+import static com.week1.game.GameController.SCALE;
+
+public class Tower {
+    public float x, y;
+    private Texture skin;
+    private int playerID;
+    private TowerType type;
+
+    public Tower(Texture t, float x, float y, TowerType towerType, int playerID) {
+        skin = t;
+        this.x = x;
+        this.y = y;
+        this.type = towerType;
+        this.playerID = playerID;
+    }
+
+    private static Pixmap blueMap = new Pixmap(SCALE, SCALE, Pixmap.Format.RGB888){{
+        setColor(Color.BLACK);
+        fill();
+    }};
+    private static Texture black = new Texture(blueMap){{
+        blueMap.dispose();
+    }};
+    
+    public Texture getSkin() {
+        return black;
+//        return skin;
+    }
+}
