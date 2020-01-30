e46a39720ee07d9ade50e6764d8111b48a9c1fad
warofthewrens <42586441+warofthewrens@users.noreply.github.com>
2020-01-29 21:37:32 -0600

    building graph

--- core/src/com/week1/game/Model/World/Block.java
+++ core/src/com/week1/game/Model/World/Block.java
@@ -1,9 +1,11 @@
 package com.week1.game.Model.World;
 
+import com.badlogic.gdx.ai.pfa.Connection;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 import static com.week1.game.GameController.PIXELS_PER_UNIT;
 
@@ -13,6 +15,8 @@ interface Block {
 
     class TerrainBlock implements Block {
         private TextureRegion textureRegion;
+        private int index;
+        private Array<Connection<TerrainBlock>> edges;
         TerrainBlock(Color color) {
             Pixmap unitPixmap = new Pixmap(PIXELS_PER_UNIT, PIXELS_PER_UNIT, Pixmap.Format.RGB888);
             unitPixmap.setColor(color);
@@ -26,7 +30,16 @@ interface Block {
         public TextureRegion getTextureRegion() {
             return textureRegion;
         }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public Array<Connection<TerrainBlock>> getConnections(){
+            return edges;
+        }
     }
+
 
     class TowerBlock implements Block {
 
