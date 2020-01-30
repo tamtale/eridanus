30c7783db451e4b719622a30d958fe8e11999888
Patrick Brady <pj.brady97@gmail.com>
2020-01-30 09:46:34 -0600

    Added cost associated with each of the creation messages

--- core/src/com/week1/game/Model/StatsConfig.java
+++ core/src/com/week1/game/Model/StatsConfig.java
@@ -5,4 +5,13 @@ public class StatsConfig {
     static final float manaRegenRate = 1;
 
     static final float playerBaseInitialHp = 100;
+
+    static public final float tempTower1Cost = 50;
+    static public final float tempTower2Cost = 75;
+    static public final float tempTowerHealth = 25;
+
+    static public final float tempMinion1Cost = 10;
+    static public final float tempMinion1Health = 10;
+
+
 }
