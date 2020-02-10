a6c1b4f692a573501d487a8283e6bb54f9a77e53
Patrick Brady <pj.brady97@gmail.com>
2020-02-10 13:48:26 -0600

    boost tower stats

--- core/src/com/week1/game/Model/StatsConfig.java
+++ core/src/com/week1/game/Model/StatsConfig.java
@@ -17,24 +17,24 @@ public class StatsConfig {
   
     // Basic
     static public final double tempTower1Cost = 75;
-    static public final double tempTower1Health = 100;
-    static public final double tempTower1Damage = 1.5;
-    static public final double tempTower1Range = 6;
+    static public final double tempTower1Health = 75;
+    static public final double tempTower1Damage = 2.5;
+    static public final double tempTower1Range = 8;
     static public final Pixmap basicTexture = new Pixmap(Gdx.files.internal("towertransparent.png"));
 
 
     // Sniper
     static public final double tempTower2Cost = 150;
     static public final double tempTower2Health = 10;
-    static public final double tempTower2Damage = 4;
+    static public final double tempTower2Damage = 5;
     static public final double tempTower2Range = 12;
     static public final Pixmap sniperTexture = new Pixmap(Gdx.files.internal("tower3.png"));
 
     // Tank
     static public final double tempTower3Cost = 100;
     static public final double tempTower3Health = 100;
-    static public final double tempTower3Damage = 1;
-    static public final double tempTower3Range = 4;
+    static public final double tempTower3Damage = 2;
+    static public final double tempTower3Range = 5;
     static public final Pixmap tankTexture = new Pixmap(Gdx.files.internal("tower3.png"));
 
     static public final double tempMinion1Cost = 10;
