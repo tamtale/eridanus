87dce0656931fb1361f6a5198edbdf07b74b5bef
tamtale <tamtale@gmail.com>
2020-02-23 19:37:33 -0600

    disallow rotation

--- core/src/com/week1/game/GameScreen.java
+++ core/src/com/week1/game/GameScreen.java
@@ -275,7 +275,7 @@ public class GameScreen implements Screen {
 
 		if (!pressedStartbtn) {
 			InputMultiplexer multiplexer = new InputMultiplexer();
-			multiplexer.addProcessor(new CameraInputController(renderer.getCamera()));
+			multiplexer.addProcessor(new GameCameraController(renderer.getCamera()));
 			// multiplexer.addProcessor(renderer.getButtonStage());
 			multiplexer.addProcessor(clickOracle);
 			Gdx.input.setInputProcessor(multiplexer);
