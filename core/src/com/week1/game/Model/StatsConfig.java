822f7041ece62980f677fc95e372bfa270bdd75b
warofthewrens <42586441+warofthewrens@users.noreply.github.com>
2020-03-11 20:35:12 -0500

    Bug fixes to master. (#82)
    * Fixing config.
    
    * fixed the out of bounds error
    
    * fixed null pointer, I think.
    
    Co-authored-by: Luke Iler <lukenetsurfer@yahoo.com>

--- core/src/com/week1/game/Model/StatsConfig.java
+++ core/src/com/week1/game/Model/StatsConfig.java
@@ -3,13 +3,13 @@ package com.week1.game.Model;
 public class StatsConfig {
     static final int PLAYERNOTASSIGNED = -1;
 
-    static final double startingMana = 9000;
+    static final double startingMana = 1000;
     static final double manaRegenRate = 1;
     static public final double towerDestructionBonus = 0.5; // This will be multiplies by the cost of the tower to determine bonus mana.
 
     static public final double placementRange = 15;
 
-    static public final double tempMinion1Cost = 20;
+    static public final double tempMinion1Cost = 10;
     static public final double tempMinion1Health = 50;
     
     static public final double tempMinionRange = 8;
