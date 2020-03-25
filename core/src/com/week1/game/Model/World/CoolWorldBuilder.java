2f1f647e9195a49b8e51952eb9c57accb90192c5
warofthewrens <42586441+warofthewrens@users.noreply.github.com>
2020-03-25 15:29:12 -0400

    flatter more height also river and lakes

--- core/src/com/week1/game/Model/World/CoolWorldBuilder.java
+++ core/src/com/week1/game/Model/World/CoolWorldBuilder.java
@@ -37,30 +37,28 @@ public class CoolWorldBuilder implements IWorldBuilder {
         for (int i = 0; i < blocks.length; i++) {
             for (int j = 0; j < blocks[0].length; j++) {
                 if (blocks[i][j][0] == null) {
-
                     float blockType = random.nextFloat();
                     if (blockType < 0.75) {
-                        blocks[i][j][0] = Block.TerrainBlock.FIREBRICK;
+                        blocks[i][j][0] = Block.TerrainBlock.ZAMBALA;
                         spread(i, j, 0, .125f, .125f, .75f, 0f,  true, Block.TerrainBlock.ZAMBALA); //zamb
                         //heightBuild(i, j, Block.TerrainBlock.ZAMBALA);
                     } else if (blockType < .98) {
-                        blocks[i][j][0] = Block.TerrainBlock.ZAMBALA;
+                        blocks[i][j][0] = Block.TerrainBlock.FIREBRICK;
                         spread(i, j, 0, .125f, .125f, .85f, 0f, true, Block.TerrainBlock.FIREBRICK); //fire
-                        //heightBuild(i, j, Block.TerrainBlock.FIREBRICK);
                     } else {
                         blocks[i][j][0] = Block.TerrainBlock.WATER;
                         float lake = random.nextFloat();
-                        float flat;
-                        if (lake < .9){
+                        float flat, ortho, diag;
+                        if (lake < .7){
                             flat = .8f;
+                            ortho = .25f;
+                            diag = 0f;
                         } else{
-                            flat = -.07f;
+                            flat = -.0125f;
+                            ortho = .175f;
+                            diag = .075f;
                         }
-                        spread(i, j, 0,.23f, .02f, .98f, flat, false, Block.TerrainBlock.WATER);
-
-//                            } else {
-//                                lake = 1;
-//                            }
+                        spread(i, j, 0,ortho, diag, .98f, flat, false, Block.TerrainBlock.WATER);
                         }
                     }
                 for (int k = 1; k < blocks[0][0].length; k++) {
@@ -69,10 +67,6 @@ public class CoolWorldBuilder implements IWorldBuilder {
 
             }
         }
-//        makePlateau(blocks, 0, 5, 0, 5);
-
-//        blocks[3][3][2] = Block.TowerBlock.REDBLOCK;
-
         return blocks;
     }
 
@@ -83,14 +77,13 @@ public class CoolWorldBuilder implements IWorldBuilder {
         float east = 0f;
         while (random.nextFloat() < sticky) {
             if (height){
-//                int lim = blocks[0][0].length - 1;
-                float incr = .997f + .0015f * k;
+                float incr = .992f + .0015f * k;
                 if (random.nextFloat() > incr && k < 2) {
                     k++;
                     diag = 0;
                     ortho = .25f;
                     sticky *= 2.7f;
-                    sticky = Math.min(sticky, .99999f);
+                    sticky = Math.min(sticky, .999f);
                     flat = .7f;
                 }
                 if (random.nextFloat() < .001 && k > 0) {
@@ -139,17 +132,13 @@ public class CoolWorldBuilder implements IWorldBuilder {
                     o--;
                     blocks[m][n][o] = block;
                 }
-                Pair<Integer, Integer> mn = updateLoc(m,n, k, block);
+                Pair<Integer, Integer> mn = updateLoc(m, n, k, block);
                 if (mn.key != m || mn.value != n){
                     sticky -=.0002f;
                 }
                 m = mn.key;
                 n = mn.value;
             }
-
-//                            } else {
-//                                lake = 1;
-//                            }
         }
     }
 
@@ -193,15 +182,6 @@ public class CoolWorldBuilder implements IWorldBuilder {
         }
         return new Pair<>(m, n);
     }
-
-//    private void heightBuild(int i, int j, Block.TerrainBlock block) {
-//        float plateau = random.nextFloat();
-//        if (plateau < .98) {
-//            blocks[i][j][0] = block;
-//        } else {
-//            spread(i, j, 1, .25f, 0f, .95f, true, block);
-//        }
-//    }
 
 
     private void makePlateau(Block[][][] blocks, int startX, int endX, int startY, int endY) {
