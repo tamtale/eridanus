91575d579d8272497a5122b57f278592f4c4a310
warofthewrens <42586441+warofthewrens@users.noreply.github.com>
2020-04-03 12:08:40 -0400

    added disttonext if statement to improve PathfindingSystem

--- core/src/com/week1/game/Model/Systems/PathfindingSystem.java
+++ core/src/com/week1/game/Model/Systems/PathfindingSystem.java
@@ -32,35 +32,36 @@ public class PathfindingSystem implements ISystem {
         PathComponent pathComponent = node.pathComponent;
         VelocityComponent velocityComponent = node.velocityComponent;
         PositionComponent positionComponent = node.positionComponent;
+        if (velocityComponent.distTraveled >= pathComponent.distanceToNext) {
+            if ((pathComponent.path == null) || (pathComponent.path.getCount() <= 0)) {
+                pathComponent.path = null;
+                velocityComponent.velocity.x = 0;
+                velocityComponent.velocity.y = 0;
+                return;
+            }
 
-        if ((pathComponent.path == null) || (pathComponent.path.getCount() <= 0)) {
-            pathComponent.path = null;
-            velocityComponent.velocity.x = 0;
-            velocityComponent.velocity.y = 0;
-            return;
+            Vector2 nextStep = pathComponent.path.get(0);
+            vecToPath.set(nextStep.x - positionComponent.position.x, nextStep.y - positionComponent.position.y);
+            int height = stateAdapter.getHeight((int) positionComponent.position.x, (int) positionComponent.position.y);
+            int nxtHeight = stateAdapter.getHeight((int) pathComponent.path.get(0).x, (int) pathComponent.path.get(0).y);
+            float blockSpeed = 1f / stateAdapter.getBlock((int) positionComponent.position.x, (int) positionComponent.position.y,
+                    height).getCost();
+            positionComponent.position.z = nxtHeight + 1;
+            float distanceToNext = (float) Math.sqrt(Math.pow(vecToPath.x, 2f) + Math.pow(vecToPath.y, 2f));
+            pathComponent.distanceToNext = distanceToNext;
+            double angle = Math.atan(vecToPath.y / vecToPath.x);
+            if (vecToPath.x < 0) {
+                angle += Math.PI;
+            } else if (vecToPath.y < 0) {
+                angle += 2 * Math.PI;
+            }
+            velocityComponent.velocity.x = blockSpeed * (float) velocityComponent.baseSpeed * (float) Math.cos(angle);
+            velocityComponent.velocity.y = blockSpeed * (float) velocityComponent.baseSpeed * (float) Math.sin(angle);
+            pathComponent.path.removeIndex(0);
+            velocityComponent.distTraveled = 0;
+            // TODO look over/redo the arrival detection mechanism
+
         }
-
-        Vector2 nextStep = pathComponent.path.get(0);
-        vecToPath.set(nextStep.x - positionComponent.position.x, nextStep.y - positionComponent.position.y);
-        int height = stateAdapter.getHeight((int) positionComponent.position.x, (int) positionComponent.position.y);
-        int nxtHeight = stateAdapter.getHeight((int) pathComponent.path.get(0).x, (int) pathComponent.path.get(0).y);
-        float blockSpeed = 1f/stateAdapter.getBlock((int) positionComponent.position.x, (int) positionComponent.position.y,
-            height).getCost();
-        positionComponent.position.z = nxtHeight + 1;
-        float distanceToNext = (float) Math.sqrt(Math.pow(vecToPath.x, 2f) + Math.pow(vecToPath.y, 2f));
-        double angle = Math.atan(vecToPath.y / vecToPath.x);
-        if (vecToPath.x < 0) {
-            angle += Math.PI;
-        } else if (vecToPath.y < 0) {
-            angle += 2 * Math.PI;
-        }
-        velocityComponent.velocity.x = blockSpeed * (float) velocityComponent.baseSpeed * (float) Math.cos(angle);
-        velocityComponent.velocity.y = blockSpeed * (float) velocityComponent.baseSpeed * (float) Math.sin(angle);
-        pathComponent.path.removeIndex(0);
-        velocityComponent.distTraveled = 0;
-        // TODO look over/redo the arrival detection mechanism
-
-
     }
 
     static class PathfindingNode {
