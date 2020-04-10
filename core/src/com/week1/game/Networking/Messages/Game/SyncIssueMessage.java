5e115b9d6efc3b631b7b64fffc33fa4a9f24c0ce
Patrick Brady <pj.brady97@gmail.com>
2020-04-10 13:25:22 -0700

    commented out all logs (#135)
    leggo

--- core/src/com/week1/game/Networking/Messages/Game/SyncIssueMessage.java
+++ core/src/com/week1/game/Networking/Messages/Game/SyncIssueMessage.java
@@ -6,26 +6,24 @@ import com.week1.game.Model.GameEngine;
 import com.week1.game.Model.GameState;
 import com.week1.game.Networking.Messages.MessageType;
 
-import java.io.*;
-import java.nio.channels.FileChannel;
 import java.util.List;
 
 public class SyncIssueMessage extends GameMessage {
     List<Integer> allHashes;
-    static BufferedWriter writer;
+//    static BufferedWriter writer;
 
     // Create the writer to write to the log file. Only need one persistent writer.
-    static {
-        try {
-            File errorFile = new File("logs/STATE-ERROR-LOG.txt");
-            FileChannel outChan = new FileOutputStream(errorFile, true).getChannel();
-            outChan.truncate(0);
-
-            writer = new BufferedWriter(new FileWriter(errorFile, true));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    static {
+//        try {
+//            File errorFile = new File("logs/STATE-ERROR-LOG.txt");
+//            FileChannel outChan = new FileOutputStream(errorFile, true).getChannel();
+//            outChan.truncate(0);
+//
+//            writer = new BufferedWriter(new FileWriter(errorFile, true));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public SyncIssueMessage(int playerID, MessageType messageTypeID, int intHash, List<Integer> allHashes) {
         super(playerID, messageTypeID, intHash);
@@ -39,20 +37,20 @@ public class SyncIssueMessage extends GameMessage {
         util.log("pjb3 - SyncIssueMessage", gameState.packState(engine.getTurn()).getGameString());
 
         // Create the message to append to the log file
-        String fileContent = "Issue with turn " + (engine.getTurn()-1) + " hashes were:";
-        for(Integer i : allHashes) {
-            fileContent += i.toString() + " ";
-        }
-        fileContent += ". Check log files in core/assets/logs\n";
-        Gdx.app.debug("pjb3 - SyncIssueMessage", fileContent);
-
-        // Add the message to the log file
-        try {
-            writer.append(fileContent);
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        String fileContent = "Issue with turn " + (engine.getTurn()-1) + " hashes were:";
+//        for(Integer i : allHashes) {
+//            fileContent += i.toString() + " ";
+//        }
+//        fileContent += ". Check log files in core/assets/logs\n";
+//        Gdx.app.debug("pjb3 - SyncIssueMessage", fileContent);
+//
+//        // Add the message to the log file
+//        try {
+//            writer.append(fileContent);
+//            writer.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return true;
     }
 }
