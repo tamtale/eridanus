6ceaa6159b98b852fa0d4626c091b212eb883df0
SimranVirk <skv2@rice.edu>
2020-04-13 15:21:47 -0400

    four minion color schemes

--- core/src/com/week1/game/Model/Entities/UnitModels.java
+++ core/src/com/week1/game/Model/Entities/UnitModels.java
@@ -8,8 +8,12 @@ public class UnitModels {
 
     public static final Model MINION;
 
+//    public static final Model WATER_MINION;
+
     static {
-        String crystalName = "minion/minion_with_weird_face.g3db";
+        String crystalName = "minion/fire_minion/minion_with_weird_face.g3db";
+//        String crystalName = "minion/water_minion/minion_with_weird_face.g3db";
+//        String crystalName = "minion/earth_minion/minion_with_weird_face.g3db";
         Initializer.assetManager.load(crystalName, Model.class);
         Initializer.assetManager.finishLoading();
         MINION = Initializer.assetManager.get(crystalName, Model.class);
