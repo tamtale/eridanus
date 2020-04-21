dc7e4683512e629456dbd623debe18199a93c24c
Tam Le <tamtale@gmail.com>
2020-04-20 22:51:45 -0500

    targeting permissions (#172)
    * targeting permissions
    - Entities can only target things according to their permission level
    - minions < towers < crystals (i.e. crystals require most permissions)
    
    * towers target towers

--- core/src/com/week1/game/Model/Components/TargetingComponent.java
+++ core/src/com/week1/game/Model/Components/TargetingComponent.java
@@ -7,18 +7,25 @@ import java.util.function.Consumer;
  * Component for an entity currently targeting another.
  */
 public class TargetingComponent extends AComponent {
+    public int permission;
     public int targetID; // -1 if no current target.
     public float range; // Targeting range.
     public boolean switchTargets; // Whether or not to switch targets when out of range.
     public TargetingStrategy strategy;
-    public TargetingComponent(int targetID, float range, boolean switchTargets, TargetingStrategy strategy) {
+    public TargetingComponent(int targetID, float range, boolean switchTargets, TargetingStrategy strategy, int permission) {
         this.targetID = targetID;
         this.range = range;
         this.switchTargets = switchTargets;
         this.strategy = strategy;
+        this.permission = permission;
     }
 
     public enum TargetingStrategy {
         ENEMY, TEAM, ALL
     }
+
+    // Permissions
+    public static int P_MINIONS = 1;
+    public static int P_MINIONS_TOWERS = 2;
+    public static int P_MINIONS_TOWERS_CRYSTALS = 3;
 }
