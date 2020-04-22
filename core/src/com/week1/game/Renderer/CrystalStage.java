55b27ed9d2dbed7b917704ce45a97c14aeeb6fc6
Patrick Brady <pj.brady97@gmail.com>
2020-04-22 14:36:27 -0700

    whatever im tired of this

--- core/src/com/week1/game/Renderer/CrystalStage.java
+++ core/src/com/week1/game/Renderer/CrystalStage.java
@@ -22,12 +22,8 @@ public class CrystalStage {
         oldWidgetHeight = Gdx.graphics.getHeight()/800f * 160;
 
         crystalWidget = new CrystalDisplayWidget();
-        crystalWidget.setLblTxt("This is a test");
-        Gdx.app.log("pjb3 - GameButtonsStage", "" + (Gdx.graphics.getWidth() * 256/800f) + " " + (Gdx.graphics.getWidth() * 256/800f));
+        crystalWidget.setLblTxt("Initial Text");
         crystalWidget.setSize(oldWidgetWidth, oldWidgetHeight);
-        Gdx.app.log("pjb3 - GameButtonsStage", "AND " + crystalWidget.getWidth() + " "+  crystalWidget.getHeight());
-//        crystalWidget.setProportion(256/ GameController.VIRTUAL_WIDTH, 128 / GameController.VIRTUAL_WIDTH);
-//        crystalWidget.setPosition( GameController.VIRTUAL_WIDTH - crystalWidget.getWidth(), GameController.VIRTUAL_HEIGHT - crystalWidget.getHeight());
         crystalWidget.resetPosition(Gdx.graphics.getWidth()/2f - oldWidgetWidth/2, Gdx.graphics.getHeight()/2f - oldWidgetHeight/2);
         stage.addActor(crystalWidget);
 
